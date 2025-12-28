775c2c0c7cb61752e56d9c48a0ef8460d0ff00aa
yukirochi <justineabanilla81@gmail.com>
2025-12-28 17:21:24 +0800

    577. Employee Bonus

---
+++ Sql/183. Customers Who Never Order.sql
@@ -0,0 +1,6 @@
+SELECT name AS Customers
+FROM Customers
+LEFT JOIN Orders
+ON Customers.id = Orders.customerId
+WHERE Orders.customerId IS NULL
+;
---
+++ Sql/577. Employee Bonus.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+SELECT name, bonus
+FROM Employee
+LEFT JOIN Bonus
+ON Employee.empId = Bonus.empId
+WHERE Bonus.bonus IS NULL OR
+Bonus.bonus < 1000
+;
