c7663d8241f1edca1bbdf81e1c8aa362966cfb93
yukirochi <justineabanilla81@gmail.com>
2026-02-25 17:52:18 +0800

    570

---
+++ Sql/585. Investments in 2016.sql
@@ -0,0 +1,17 @@
+-- Write your PostgreSQL query
+
+SELECT ROUND(SUM(tiv_2016)::numeric, 2) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+) AND (lat,lon) IN (
+    SELECT lat,lon
+    FROM Insurance
+    GROUP by lat, lon
+    HAVING COUNT(*) = 1
+)
+
+;
